79fd04b20517b3df0bcb1ebb6e76be98154751ea
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-07-11 11:27:52 -0400

    home route now working

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -2,5 +2,31 @@ var path = require('path');
 
 module.exports = function (app){
 
-	//app.get
+	// HTML GET Requests
+	// Below code handles when users "visit" a page. 
+	// In each of the below cases the user is shown an HTML page of content
+	// ---------------------------------------------------------------------------
+
+	app.get('/tables', function(req, res){
+		console.log(index_plus(tdata));
+		tdata.forEach(function(tdata, index, array){ //for each item in the tables array you are added a number property
+			tdata.number = index + 1;
+		});
+
+		wdata.forEach(function(wdata, index, array){
+			wdata.number = index + 1;
+		});
+		res.render('tables', {table_data: tdata, waiting_data: wdata});
+	});
+
+	app.get('/reserve', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/reserve.html'));
+	});
+
+	// If no matching route is found default to home
+	app.use(function(req, res){
+		// res.sendFile(path.join(__dirname + '/../public/home.html'));
+		res.render('home');
+	});
+	
 }
--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 //Required Node Modules
 	//handlebars
-	//express
-	//express-handlebars
+	var express = require('express');
+	var exphbs = require('express-handlebars');
 	//sessions
-	//path
-	//body-parser
+	var path = require('path');
+	var bodyParser = require('body-parser');
 	
 
 //Port
+var app = express();
 var PORT = process.env.PORT || 8000;
+
+
+// Handlebars
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+
+// BodyParser
+// BodyParser makes it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs. 
+// ================================================================================
+require('./app/routing/html-routes.js')(app);
+
+
 
 //Listener
 app.listen(PORT, function(){
