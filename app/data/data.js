e39eb6eb6f4b443964778713f3d6bcaa6bd55531
PoffDev <joshuapoffenberger@gmail.com>
2016-07-12 17:08:01 -0400

    api start

--- app/data/data.js
+++ app/data/data.js
@@ -1,1 +1,14 @@
-console.log('working');
+$(document).ready(function(){
+
+	console.log('working');
+
+	var NPapi = "https://projects.propublica.org/nonprofits/api/v1";
+
+	$.ajax({
+		url: api,
+		method: 'GET'
+	})
+		.done(function(nonprofits){
+			console.log(nonprofits);
+		})
+})
