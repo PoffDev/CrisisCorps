d97b38279c21956cdd09443592601ac84c4fe30a
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-07-14 21:30:18 -0400

    started working on these files

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -7,10 +7,6 @@ module.exports = function (app){
 	// In each of the below cases the user is shown an HTML page of content
 	// ---------------------------------------------------------------------------
 
-	app.get('/about', function(req, res){
-		res.render('about');
-	});
-
 	app.get('/admin', function(req, res){
 		res.render('admin');
 	});
@@ -23,16 +19,8 @@ module.exports = function (app){
 		res.render('crisis');
 	});
 
-	app.get('/friends', function(req, res){
-		res.render('friends');
-	});
-
 	app.get('/profile', function(req, res){
 		res.render('profile');
-	});
-
-	app.get('/resources', function(req, res){
-		res.render('resources');
 	});
 
 	app.get('/signin', function(req, res){
