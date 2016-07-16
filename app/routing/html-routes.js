4f0f05b5aad852b2d1ebb6513a836ebbc098ef11
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-07-16 08:13:59 -0400

    title pages now appearing dynamically on all pages

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -8,41 +8,41 @@ module.exports = function (app){
 	// ---------------------------------------------------------------------------
 
 	app.get('/crisis', function(req, res){
-		res.render('crisis');
+		res.render('crisis', {title: 'Crisis Manager'});
 	});
 
 	app.get('/profile', function(req, res){
-		res.render('profile');
+		res.render('profile', {title: 'Profile'});
 	});
 
 	app.get('/signin', function(req, res){
-		res.render('signin');
+		res.render('signin', {title: 'Sign In'});
 	});
 
 	app.get('/signup', function(req, res){
-		res.render('signup');
+		res.render('signup', {title: 'Sign Up'});
 	});
 
 	app.get('/tasks', function(req, res){
-		res.render('tasks');
+		res.render('tasks', {title: 'Tasks List'});
 	});
 
 	app.get('/task', function(req, res){
-		res.render('task');
+		res.render('task', {title: 'Task'});
 	});
 
 	app.get('/dashboard', function(req, res){
-		res.render('dashboard');
+		res.render('dashboard', {title: 'Dashboard'});
 	});
 
 	app.get('/corp', function(req, res){
-		res.render('corp');
+		res.render('corp', {title: 'Corporation List'});
 	});
 
 	// If no matching route is found default to home
 	app.use(function(req, res){
 		// res.sendFile(path.join(__dirname + '/../public/home.html'));
-		res.render('home');
+		res.render('home', {title: 'Home'});
 	});
 
 }
