b72a7527a8014739aaa402c5037692bacbd4dd82
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-07-16 08:35:59 -0400

    breadcrumbs working but still need nested breadcrumbs to work for tasks/task

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -8,41 +8,68 @@ module.exports = function (app){
 	// ---------------------------------------------------------------------------
 
 	app.get('/crisis', function(req, res){
-		res.render('crisis', {title: 'Crisis Manager'});
+		res.render('crisis', {
+			title: 'Crisis Manager',
+			link: 'crisis'
+		});
 	});
 
 	app.get('/profile', function(req, res){
-		res.render('profile', {title: 'Profile'});
+		res.render('profile', {
+			title: 'Profile',
+			link: 'profile'
+		});
 	});
 
 	app.get('/signin', function(req, res){
-		res.render('signin', {title: 'Sign In'});
+		res.render('signin', {
+			title: 'Sign In',
+			link: 'signin'
+		});
 	});
 
 	app.get('/signup', function(req, res){
-		res.render('signup', {title: 'Sign Up'});
+		res.render('signup', {
+			title: 'Sign Up',
+			link: 'signup'
+		});
 	});
 
 	app.get('/tasks', function(req, res){
-		res.render('tasks', {title: 'Tasks List'});
+		res.render('tasks', {
+			title: 'Tasks List',
+			link: 'tasks'
+		});
 	});
 
 	app.get('/task', function(req, res){
-		res.render('task', {title: 'Task'});
+		res.render('task', {
+			title: 'Task',
+			link: 'task'
+		});
 	});
 
 	app.get('/dashboard', function(req, res){
-		res.render('dashboard', {title: 'Dashboard'});
+		res.render('dashboard', {
+			title: 'Dashboard',
+			link: 'dashboard'
+		});
 	});
 
 	app.get('/corp', function(req, res){
-		res.render('corp', {title: 'Corporation List'});
+		res.render('corp', {
+			title: 'Corporation List',
+			link: 'corp'
+		});
 	});
 
 	// If no matching route is found default to home
 	app.use(function(req, res){
 		// res.sendFile(path.join(__dirname + '/../public/home.html'));
-		res.render('home', {title: 'Home'});
+		res.render('home', {
+			title: 'Home',
+			link: 'home'
+		});
 	});
 
 }
