a1f6ff622adda8f183bd674c55598d453c61bf6a
Joshua Cruey <Joshua@crueyphotography.com>
2016-07-17 16:24:20 -0400

    revised seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -41,19 +41,19 @@ INSERT INTO Users (userName, emailAddress, password, contactNum, userType) VALUE
 INSERT INTO corporateMembers (userID, companyName, donationDesc) VALUES (LAST_INSERT_ID(), 'Builder Depot', 'We can provide building materials to places that need to reinforce and repair damaged buildings. We can also provide 500 bottled waters and 200 hot dogs. We can also send a food vending cart free of charge for 3 hours during any food drives or pantry efforts that happen.');
 
 -- TASKS
-INSERT INTO availableTasks (taskTitle, taskDescript, volsNeeded, contactName, contactNum, taskAddress, taskAddress2, taskCity, taskState, taskZip)
+INSERT INTO availableTasks (taskTitle, taskDescript, volsNeeded, volsRemaining contactName, contactNum, taskAddress, taskAddress2, taskCity, taskState, taskZip)
 
-VALUES ('Blood Donation at ORMC', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like.', 14, 'Samantha Pierce', 4075555555, '232 N Orange Ave.', 'building 2 suite 100', 'Orlando', 'FL', 32807),
+VALUES ('Blood Donation at ORMC', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like.', 14, 14, 'Samantha Pierce', 4075555555, '232 N Orange Ave.', 'building 2 suite 100', 'Orlando', 'FL', 32807),
 
-('Blood Donation at Quest Diagnostic Dr. Phillips', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.', 123, 'Barbara Blithe', 4078888888, '222 Phillips St.', 'suite 250', 'Orlando', 'FL', 32199),
+('Blood Donation at Quest Diagnostic Dr. Phillips', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.', 123, 123, 'Barbara Blithe', 4078888888, '222 Phillips St.', 'suite 250', 'Orlando', 'FL', 32199),
 
-('Volunteers at the Citrus Bowl', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.', 55, 'Miguel Olavido', 3213456789, '19007 Paramore St.', '', 'Orlando', 'FL', 32806),
+('Volunteers at the Citrus Bowl', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.', 55, 55, 'Miguel Olavido', 3213456789, '19007 Paramore St.', '', 'Orlando', 'FL', 32806),
 
-('Volunteers at BrightHouse Stadium UCF', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.', 23, 'Steve Hallow', 4071112234, '444 ucf Way', 'building 25', 'Orlando', 'FL', 32801),
+('Volunteers at BrightHouse Stadium UCF', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.', 23, 23, 'Steve Hallow', 4071112234, '444 ucf Way', 'building 25', 'Orlando', 'FL', 32801),
 
-('Medicall Background Volunteers Needed at Orange Ave and Wall St', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.', 3, 'Kate Grills', 4071110090, '3 N Orange Ave', '', 'Orlando', 'FL', 32816),
+('Medicall Background Volunteers Needed at Orange Ave and Wall St', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.', 3, 3, 'Kate Grills', 4071110090, '3 N Orange Ave', '', 'Orlando', 'FL', 32816),
 
-('Soup Kitches Staff at Lake Eola', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.', 37, 'Victor Pedilla', 4075649921, '4589 Some St.', 'Suite 500', 'Orlando', 'FL', 32088);
+('Soup Kitches Staff at Lake Eola', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.', 37, 37, 'Victor Pedilla', 4075649921, '4589 Some St.', 'Suite 500', 'Orlando', 'FL', 32088);
 
 -- CRISIS
 
