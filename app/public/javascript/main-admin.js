6efe7c8468728238fe944618cd273906f26acecc
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-07-17 19:58:03 -0400

    admin nav now responds to 1024px media query with hamburger menu

--- app/public/javascript/main-admin.js
+++ app/public/javascript/main-admin.js
@@ -1,3 +1,22 @@
+// admin nav toggle
+
+function adminNavToggle() {
+	
+	// grab the ul element holding the admin navigation on left
+	var nav_ul = document.getElementById('admin-nav-toggle');
+
+	// toggle the hidden class. It's set to hidden from page load so the nav doesn't take up the entire screen when first loaded
+	$(nav_ul).toggleClass('hidden');
+
+} // end adminNavToggle()
+
+// click event on the hamburger menu item that appears at 1024px screen size
+$('#nav-toggle').on('click', function() {
+	
+	// call tha adminNavToggle function
+	adminNavToggle();
+
+}); // end nav-toggle click event
 function volCanvas(total_vol_positions) {
 
 	// get number of commited volunteers from what's displayed on the dashboard	
