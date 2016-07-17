00254b9aaa96efe71b1a6aa0c056667ec98dc3a2
Jennifer Rose Hernandez <jennrosedev@gmail>
2016-07-17 16:52:58 -0400

    Orm update

--- config/orm.js
+++ config/orm.js
@@ -5,18 +5,26 @@ var connection = require('./connection.js');
 
 var orm = {
     //form data entry queries
-    members: function(userName, emailAddress, password, contactNum, userType) {
-        var queryString = 'INSERT INTO Users (userName, emailAddress, password, contactNum, userType) VALUES (?, ?, ?, ?, ?)';
-        var vals = [userName, emailAddress, password, contactNum, userType];
+    Users: function(userName, emailAddress, password, userType) {
+        var queryString = 'INSERT INTO users (userName, emailAddress, password, userType) VALUES (?, ?, ?, ?)';
+        var vals = [userName, emailAddress, password, userType];
          connection.query(queryString, vals, function(err, result) {
                 if (err) throw err;
                 console.log(result);
             });
         },
+    members: function(userId, contactNumber, bloodType) {
+        var queryString = 'INSERT INTO users (userId, contactNumber, bloodType) VALUES(?, ?, ?)';
+        var vals = [userId, bloodType];
+        connection.query(queryString, vals, function(err, result){
+            if(err) throw err;
+            console.log(result);
+        });
+    },
 
-    corporateMembers: function(companyName, donationDesc){ 
-    var queryString = 'INSERT INTO corporateMembers (companyName, DonationDesc) VALUES (?, ?)';
-    var vals = [companyName, donationDesc];
+    corporateMembers: function(companyName, contactNum, donationDesc){ 
+    var queryString = 'INSERT INTO corporateMembers (companyName, contactNum, donationDesc) VALUES (?, ?)';
+    var vals = [companyName, contactNum, donationDesc];
 
          connection.query(queryString, vals, function(err, result) {
                 if (err) throw err;
@@ -26,22 +34,15 @@ var orm = {
 
     //admin panel task
 
-    AvailableTasks: function(taskTitle, taskDesc, volsNeeded, contactName, contactNum, taskAddress, taskAddress2, taskCity, taskState, taskZip){ 
-    var queryString = 'INSERT INTO AvailableTasks (taskTitle, taskDesc, volsNeeded, contactName, contactNum, taskAddress, taskAddress2, taskCity, taskState, taskZip) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    var vals = [taskTitle, taskDesc, volsNeeded, contactName, contactNum, taskAddress, taskAddress2, taskCity, taskState, taskZip];
+    AvailableTasks: function(taskName, taskDescript, volsNeeded, volsRemaining, contactName, contactNum, taskAddress, taskAddress2, taskCity, taskState, taskZip){ 
+    var queryString = 'INSERT INTO AvailableTasks (taskName, taskDescript, volsNeeded, volsRemaining, contactName, contactNum, taskAddress, taskAddress2, taskCity, taskState, taskZip) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    var vals = [taskName, taskDescript, volsNeeded, volsRemaining, contactName, contactNum, taskAddress, taskAddress2, taskCity, taskState, taskZip];
 
             connection.query(queryString, vals, function(err, result) {
                 if (err) throw err;
                 console.log(result);
             });
         },
-
-    // findWhoHasMost: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
-    //     var queryString = 'SELECT ' + tableOneCol + ', COUNT(' + tableOneCol + ') AS count FROM ' + tableOne + ' LEFT JOIN ' + tableTwo + ' ON ' + tableTwo + '.' + tableTwoForeignKey + '= ' + tableOne + '.id GROUP BY ' + tableOneCol + ' ORDER BY count desc LIMIT 1';
-    //     connection.query(queryString, function(err, result) {
-    //         console.log(result);
-    //     });
-    // }
 };
 
 module.exports = orm;
