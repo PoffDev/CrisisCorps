e2c13d0c8be4e6ce57eaea2f5f7bf02cd1057c9b
PoffDev <joshuapoffenberger@gmail.com>
2016-07-19 17:23:41 -0400

    server.js

--- server.js
+++ server.js
@@ -2,10 +2,16 @@
 //handlebars
 var express = require('express');
 var exphbs = require('express-handlebars');
+//login
+var flash = require('connect-flash');
+var passport = require('passport');
+var session = require('express-session');
 //sessions
 var path = require('path');
 var bodyParser = require('body-parser');
-	
+
+//check this if not working...
+var orm = require('./config/orm.js')	
 
 //Port
 var app = express();
@@ -22,6 +28,15 @@ app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 // access to the public folder
 app.use(express.static('app/public'));
 
+//session is used to keep the user logged in 
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true}))
+
+//flash is used to show a message on an incorrect login
+app.use(flash());
+
+//passport middleware methods
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Handlebars
 app.engine('handlebars', exphbs({
@@ -48,7 +63,8 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 require('./app/routing/api-routes.js')(app);
 require('./app/routing/html-routes.js')(app);
 
-
+//check these if they do not work.
+orm.connectToDB();
 
 //Listener
 app.listen(PORT, function(){
